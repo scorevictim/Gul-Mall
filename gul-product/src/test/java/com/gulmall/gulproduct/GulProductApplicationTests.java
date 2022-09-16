8b73649eb1a4bc662f591e4d492f6ba2bd39d019
scorevictim <1031161009sean@gmail.com>
2022-09-16 02:26:08 -0700

    unit test

--- gul-product/src/test/java/com/gulmall/gulproduct/GulProductApplicationTests.java
+++ gul-product/src/test/java/com/gulmall/gulproduct/GulProductApplicationTests.java
@@ -1,13 +1,28 @@
 package com.gulmall.gulproduct;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.gulmall.gulproduct.entity.BrandEntity;
+import com.gulmall.gulproduct.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GulProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
     @Test
     void contextLoads() {
+
+        BrandEntity brandEntity = new BrandEntity();
+
+        brandEntity.setDescript("haha");
+        brandEntity.setName("Huawei");
+        brandService.save(brandEntity);
+        System.out.println(brandEntity.getName());
+
+
     }
 
 }
