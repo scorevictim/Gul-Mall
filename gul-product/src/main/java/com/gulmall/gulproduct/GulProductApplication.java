0149ac6e9288a4d24a55f96f397ab46e0208e110
scorevictim <1031161009sean@gmail.com>
2022-09-16 01:21:57 -0700

    add mapper for DAO

--- gul-product/src/main/java/com/gulmall/gulproduct/GulProductApplication.java
+++ gul-product/src/main/java/com/gulmall/gulproduct/GulProductApplication.java
@@ -1,8 +1,10 @@
 package com.gulmall.gulproduct;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.gulmall.gulproduct.dao")
 @SpringBootApplication
 public class GulProductApplication {
 
