ff9159f6a01aef2eb75283c540095d5dcaf186d7
scorevictim <1031161009sean@gmail.com>
2022-09-17 01:49:06 -0700

    gateway module init

---
+++ gul-gateway/src/main/java/com/gulmall/gulgateway/GulGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gulmall.gulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GulGatewayApplication.class, args);
+	}
+
+}
