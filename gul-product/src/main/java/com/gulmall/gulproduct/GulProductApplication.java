9b5bec26d61b21b89906ef7ec7e20acfae43e249
scorevictim <1031161009sean@gmail.com>
2022-09-16 21:56:52 -0700

    add service to nacos discovery

--- gul-product/src/main/java/com/gulmall/gulproduct/GulProductApplication.java
+++ gul-product/src/main/java/com/gulmall/gulproduct/GulProductApplication.java
@@ -1,10 +1,10 @@
 package com.gulmall.gulproduct;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@MapperScan("com.gulmall.gulproduct.dao")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulProductApplication {
 
