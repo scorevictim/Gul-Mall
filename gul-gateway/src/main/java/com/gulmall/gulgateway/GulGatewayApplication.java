4fa7c0ca00151d605e97ec8e94eae3120e9885db
scorevictim <1031161009sean@gmail.com>
2022-09-17 01:58:02 -0700

    gateway module init

--- gul-gateway/src/main/java/com/gulmall/gulgateway/GulGatewayApplication.java
+++ gul-gateway/src/main/java/com/gulmall/gulgateway/GulGatewayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GulGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GulGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GulGatewayApplication.class, args);
+    }
 
 }
--- gul-gateway/src/test/java/com/gulmall/gulgateway/GulGatewayApplicationTests.java
+++ gul-gateway/src/test/java/com/gulmall/gulgateway/GulGatewayApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GulGatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
